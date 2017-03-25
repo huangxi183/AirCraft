737a0370e7a9c898babfc649fc252d1f1615c6cc
huangxi183 <huangxi183@gmail.com>
2017-03-25 16:29:26 -0400

    initial board

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,4 +1,8 @@
-type Board = string[][];
+type Board = number[][];
+//>=0:blank
+// == -1:grey
+//<-1:red
+
 interface BoardDelta {
   row: number;
   col: number;
@@ -7,6 +11,13 @@ type IProposalData = BoardDelta;
 interface IState {
   board: Board;
   delta: BoardDelta;
+}
+// head info
+interface HeadPosi{
+  index: number;
+  x:number;
+  y:number;
+  direct:number;
 }
 
 import gameService = gamingPlatform.gameService;
@@ -17,24 +28,156 @@ import log = gamingPlatform.log;
 import dragAndDropService = gamingPlatform.dragAndDropService;
 
 module gameLogic {
-  export const ROWS = 3;
-  export const COLS = 3;
-
-  /** Returns the initial TicTacToe board, which is a ROWSxCOLS matrix containing ''. */
+  export const ROWS = 6;
+  export const COLS = 6;
+  let head : HeadPosi = {index : Math.floor(Math.random() * 20) + 1, x :0, y :0, direct :0};
+  /** Returns the initial AirCraft board, which is a ROWSxCOLS matrix containing ''. */
   export function getInitialBoard(): Board {
     let board: Board = [];
+    // generate random craft head
+    //head.index = Math.floor(Math.random() * 20) + 1;
+
+    //choose a direction based on the head position
+    if(head.index >=17 && head.index <=20){
+      let rand: number = Math.floor(Math.random() * 2) +1;
+      switch (head.index){
+        case 17:
+          if(rand == 1){
+            head.direct = 1;
+          }else{
+            head.direct =2;
+          }
+          head.x = 2;
+          head.y = 2;
+          break;
+        case 18:
+          if(rand == 1){
+            head.direct = 1;
+          }else{
+            head.direct =3;
+          }
+          head.x = 3;
+          head.y = 2;
+          break;
+        case 19:
+          if(rand == 1){
+            head.direct = 2;
+          }else{
+            head.direct =4;
+          }
+          head.x = 2;
+          head.y = 3;
+          break;
+        case 20:
+          if(rand == 1){
+            head.direct = 3;
+          }else{
+            head.direct = 4;
+          }
+          head.x = 3;
+          head.y = 3;
+          break;
+      }
+    }else if(head.index >=1 && head.index <=4){
+      head.direct = 1;
+      head.x = (head.index ===1 ||head.index == 3)?2:3;
+      head.y = (head.index ===1 ||head.index == 2)?0:1;
+    }else if(head.index >= 5 && head.index <=8){
+      head.direct = 2;
+      head.x = (head.index ===5 ||head.index == 7)?0:1;
+      head.y = (head.index ===5 ||head.index == 6)?2:3;
+    }else if(head.index >=9 && head.index <= 12){
+      head.direct = 3;
+      head.x = (head.index ===9 ||head.index == 11)?4:5;
+      head.y = (head.index ===9 ||head.index == 10)?2:3;
+    }else if(head.index >= 13 && head.index <=16){
+      head.direct =4;
+      head.x = (head.index ===13 ||head.index == 15)?2:3;
+      head.y = (head.index ===13 ||head.index == 14)?4:5;
+    }
+
     for (let i = 0; i < ROWS; i++) {
       board[i] = [];
       for (let j = 0; j < COLS; j++) {
-        board[i][j] = '';
+        board[i][j] = 0;
       }
     }
+    let x = head.x;
+    let y = head.y;
+
+    //initial aircraft in board
+    switch (head.direct){
+      case 1:
+        board[x][y] = 10;
+        //body
+        board[x][y+1] = 5;
+        board[x][y+2] = 5;
+        //wing
+        board[x-2][y+1] = 2;
+        board[x-1][y+1] = 2;
+        board[x+1][y+1] = 2;
+        board[x+2][y+1] = 2;
+        //tail
+        board[x-1][y+3] = 3;
+        board[x][y+3] = 3;
+        board[x+1][y+3] = 3;
+        break;
+      case 2:
+        board[x][y] = 10;
+        //body
+        board[x+1][y] = 5;
+        board[x+2][y] = 5;
+        //wing
+        board[x+1][y-2] = 2;
+        board[x+1][y-1] = 2;
+        board[x+1][y+1] = 2;
+        board[x+1][y+2] = 2;
+        //tail
+        board[x+3][y-1] = 3;
+        board[x+3][y+1] = 3;
+        board[x+3][y] = 3;
+        break;
+      case 3:
+        board[x][y] = 10;
+        //body
+        board[x-1][y] = 5;
+        board[x-2][y] = 5;
+        //wing
+        board[x-1][y-2] = 2;
+        board[x-1][y-1] = 2;
+        board[x-1][y+1] = 2;
+        board[x-1][y+2] = 2;
+        //tail
+        board[x-3][y-1] = 3;
+        board[x-3][y+1] = 3;
+        board[x-3][y] = 3;
+        break;
+      case 4:
+        board[x][y] = 10;
+        //body
+        board[x][y-1] = 5;
+        board[x][y-2] = 5;
+        //wing
+        board[x-2][y-1] = 2;
+        board[x-1][y-1] = 2;
+        board[x+1][y-1] = 2;
+        board[x+2][y-1] = 2;
+        //tail
+        board[x-1][y-3] = 3;
+        board[x][y-3] = 3;
+        board[x+1][y-3] = 3;
+        break;
+    }
+
+
     return board;
   }
+
 
   export function getInitialState(): IState {
     return {board: getInitialBoard(), delta: null};
   }
+
 
   /**
    * Returns true if the game ended in a tie because there are no empty cells.
@@ -43,18 +186,7 @@ module gameLogic {
    *      ['X', 'O', 'O'],
    *      ['O', 'X', 'X']]
    */
-  function isTie(board: Board): boolean {
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        if (board[i][j] === '') {
-          // If there is an empty cell then we do not have a tie.
-          return false;
-        }
-      }
-    }
-    // No empty cells, so we have a tie!
-    return true;
-  }
+  
 
   /**
    * Return the winner (either 'X' or 'O') or '' if there is no winner.

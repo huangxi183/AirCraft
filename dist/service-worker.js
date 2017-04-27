49197c6ccd33882dca1f7360ac566aeab49ed6a3
huangxi183 <huangxi183@gmail.com>
2017-04-27 19:00:04 -0400

    a

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-26T22:44:42.893Z';
+var CACHE_NAME = 'cache-v2017-04-27T22:59:49.817Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(

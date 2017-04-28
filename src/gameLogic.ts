fd751e16066ee849b46d79c67c4e3972ce7be48a
范欧侃 <of394@nyu.edu>
2017-04-28 18:21:17 -0400

    fdaf

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -243,10 +243,12 @@ module gameLogic {
       turnIndex = -1;
       temp_score[turnIndexBeforeMove] = 10 - points_to_win[turnIndexBeforeMove];
       temp_score[1-turnIndexBeforeMove] = 10 - points_to_win[1-turnIndexBeforeMove];
+      /*-------
       points_to_win[0] = 10;
       points_to_win[1] = 10;
       head[0] = getInitialHP();
       head[1] = getInitialHP();
+      -------*/
     }
     else {
       turnIndex = 1 - turnIndex;

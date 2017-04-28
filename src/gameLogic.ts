60c39438bf6e073054f52b37e6d630d9e4893f58
范欧侃 <of394@nyu.edu>
2017-04-28 15:51:30 -0400

    DFasfa

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -201,7 +201,9 @@ module gameLogic {
    *      ['X', '', '']]
    */
   function winOrNot(turnIndexBeforeMove: number): boolean {
-    if (points_to_win[turnIndexBeforeMove] <= 0) return true;
+    if (points_to_win[turnIndexBeforeMove] <= 0) {
+      return true;
+    }
     else return false;
   }
 
@@ -241,6 +243,10 @@ module gameLogic {
       turnIndex = -1;
       temp_score[turnIndexBeforeMove] = 10 - points_to_win[turnIndexBeforeMove];
       temp_score[1-turnIndexBeforeMove] = 10 - points_to_win[1-turnIndexBeforeMove];
+      points_to_win[0] = 10;
+      points_to_win[1] = 10;
+      head[0] = getInitialHP();
+      head[1] = getInitialHP();
     }
     else {
       turnIndex = 1 - turnIndex;

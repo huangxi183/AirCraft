68ca6f530b9444ebaaa195153dbb51c94db2d104
范欧侃 <of394@nyu.edu>
2017-05-06 19:41:53 -0400

    Fdafdfa

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-05-06T19:16:30.660Z';
+var CACHE_NAME = 'cache-v2017-05-06T23:41:40.807Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
